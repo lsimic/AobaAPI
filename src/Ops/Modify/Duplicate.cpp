c6e5f4f542e3697a0bfa86618261deff1f5d587f
unknown <luka.simic51@gmail.com>
2021-03-20 15:38:43 +0100

    use intern flags in operator, fix flaxs and indexes not reset at end of operator

--- src/Ops/Modify/Duplicate.cpp
+++ src/Ops/Modify/Duplicate.cpp
@@ -17,7 +17,7 @@ const DuplicateResult Duplicate(Core::Mesh* m, const std::vector<Core::Vert*>& v
         Core::MakeVert(m, newv);
 
         newv->co = verts.at(i)->co;
-        verts.at(i)->flags = COPIED;
+        verts.at(i)->flagsIntern = COPIED;
 
         verts.at(i)->index = vertIdx;
         vertIdx++;
@@ -34,34 +34,34 @@ const DuplicateResult Duplicate(Core::Mesh* m, const std::vector<Core::Vert*>& v
         Core::Edge* newe = new Core::Edge();
         Core::Edge* current = edges.at(i);
         current->index = edgeIdx;
-        current->flags = COPIED;
+        current->flagsIntern = COPIED;
         edgeIdx++;
 
         Core::Vert* v1;
         Core::Vert* v2;
 
-        if(current->Verts().at(0)->flags & COPIED) {
+        if(current->Verts().at(0)->flagsIntern & COPIED) {
             v1 = newVerts.at(current->Verts().at(0)->index);
         } else {
             v1 = new Core::Vert();
             Core::MakeVert(m, v1);
 
             v1->co = current->Verts().at(0)->co;
-            current->Verts().at(0)->flags = COPIED;
+            current->Verts().at(0)->flagsIntern = COPIED;
 
             current->Verts().at(0)->index = vertIdx;
             vertIdx++;
             newVerts.push_back(v1);
         }
 
-        if(current->Verts().at(1)->flags & COPIED) {
+        if(current->Verts().at(1)->flagsIntern & COPIED) {
             v2 = newVerts.at(current->Verts().at(1)->index);
         } else {
             v2 = new Core::Vert();
             Core::MakeVert(m, v2);
 
             v2->co = current->Verts().at(1)->co;
-            current->Verts().at(1)->flags = COPIED;
+            current->Verts().at(1)->flagsIntern = COPIED;
 
             current->Verts().at(1)->index = vertIdx;
             vertIdx++;
@@ -90,14 +90,14 @@ const DuplicateResult Duplicate(Core::Mesh* m, const std::vector<Core::Vert*>& v
 
             for(int k = 0; k < loops.size(); ++k) {
                 // check if loop vert is copied, push to loopverts
-                if(loops.at(k)->Vert()->flags & COPIED) {
+                if(loops.at(k)->Vert()->flagsIntern & COPIED) {
                     loopVerts.push_back(newVerts.at(loops.at(k)->Vert()->index));
                 } else {
                     Core::Vert* newv = new Core::Vert();
                     Core::MakeVert(m, newv);
 
                     newv->co = loops.at(k)->Vert()->co;
-                    loops.at(k)->Vert()->flags = COPIED;
+                    loops.at(k)->Vert()->flagsIntern = COPIED;
                     loops.at(k)->Vert()->index = vertIdx;
                     vertIdx++;
                     newVerts.push_back(newv);
@@ -105,40 +105,40 @@ const DuplicateResult Duplicate(Core::Mesh* m, const std::vector<Core::Vert*>& v
                 }
 
                 // check if loop edge is copied, push to loopedges
-                if(loops.at(k)->Edge()->flags & COPIED) {
+                if(loops.at(k)->Edge()->flagsIntern & COPIED) {
                     loopEdges.push_back(newEdges.at(loops.at(k)->Edge()->index));
                 } else {
                     Core::Edge* newe = new Core::Edge();
                     Core::Edge* current = loops.at(k)->Edge();
                     current->index = edgeIdx;
-                    current->flags = COPIED;
+                    current->flagsIntern = COPIED;
                     edgeIdx++;
 
                     Core::Vert* v1;
                     Core::Vert* v2;
 
-                    if(current->Verts().at(0)->flags & COPIED) {
+                    if(current->Verts().at(0)->flagsIntern & COPIED) {
                         v1 = newVerts.at(current->Verts().at(0)->index);
                     } else {
                         v1 = new Core::Vert();
                         Core::MakeVert(m, v1);
 
                         v1->co = current->Verts().at(0)->co;
-                        current->Verts().at(0)->flags = COPIED;
+                        current->Verts().at(0)->flagsIntern = COPIED;
 
                         current->Verts().at(0)->index = vertIdx;
                         vertIdx++;
                         newVerts.push_back(v1);
                     }
 
-                    if(current->Verts().at(1)->flags & COPIED) {
+                    if(current->Verts().at(1)->flagsIntern & COPIED) {
                         v2 = newVerts.at(current->Verts().at(1)->index);
                     } else {
                         v2 = new Core::Vert();
                         Core::MakeVert(m, v2);
 
                         v2->co = current->Verts().at(1)->co;
-                        current->Verts().at(1)->flags = COPIED;
+                        current->Verts().at(1)->flagsIntern = COPIED;
 
                         current->Verts().at(1)->index = vertIdx;
                         vertIdx++;
@@ -164,6 +164,47 @@ const DuplicateResult Duplicate(Core::Mesh* m, const std::vector<Core::Vert*>& v
     result.verts = newVerts;
     result.faces = newFaces;
     result.edges = newEdges;
+
+    // TODO: perhaps this could be done faster
+    // clear all flags and indexes
+    for(int i = 0; i < verts.size(); ++i) {
+        verts.at(i)->flagsIntern = 0;
+        verts.at(i)->index = 0;
+        vertIdx++;
+    }
+
+    for(int i = 0; i < edges.size(); ++i) {
+        edges.at(i)->flagsIntern = 0;
+        edges.at(i)->index = 0;
+
+        edges.at(i)->Verts().at(0)->index = 0;
+        edges.at(i)->Verts().at(0)->flagsIntern = 0;
+        edges.at(i)->Verts().at(1)->index = 0;
+        edges.at(i)->Verts().at(1)->flagsIntern = 0;
+    }
+
+    for(int i = 0; i < faces.size(); ++i) {
+        std::vector<Core::FaceLoopList*> faceLoopLists = faces.at(i)->LoopLists();
+
+        for(int j = 0; j < faceLoopLists.size(); ++j) {
+            std::vector<Core::Loop*> loops = faceLoopLists.at(j)->Loops();
+
+            for(int k = 0; k < loops.size(); ++k) {
+                loops.at(k)->Vert()->flagsIntern = 0;
+                loops.at(k)->Vert()->index = 0;
+
+                if(loops.at(k)->Edge()->flagsIntern) {
+                    loops.at(k)->Edge()->Verts().at(0)->index = 0;
+                    loops.at(k)->Edge()->Verts().at(0)->flagsIntern = 0;
+                    loops.at(k)->Edge()->Verts().at(1)->index = 0;
+                    loops.at(k)->Edge()->Verts().at(1)->flagsIntern = 0;
+                }
+
+                loops.at(k)->Edge()->flagsIntern = 0;
+                loops.at(k)->Edge()->index = 0;
+            }
+        }
+    }
 
     return result;
 }
